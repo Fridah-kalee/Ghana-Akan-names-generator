7224bbf1481d38eb23edf108dff915f094273b85
Fridah-kalee <fridahkalee@gmail.com>
2022-03-06 11:14:43 +0300

    finding variables

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ var femaleNames = ['Akosua',  'Adwoa', 'Abenaa', 'Akua',  'Yaa', 'Afua', 'Ama'];
 //alert to enter a valid one
 
 function check(){
-    var gender = document.getElementById('gender').value;
-    var day = document.getElementById('day').value;
+    var gender = document.getElementById("gender").value;
+    var day = document.getElementById("day").value;
     var dd = parseInt(day);
-    var month = document.getElementById('month').value;
+    var month = document.getElementById("month").value;
     var mm = parseInt(month);
-    var year = document.getElementById('year').value;
+    var year = document.getElementById("year").value;
     var yy = parseInt(year.substr(2.4));
-    var yy = parseInt(year.substr(0.2));
+    var cc = parseInt(year.substr(0.2));
 
-   var dayWeek = Maths.abs(parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)) %7;
+   var birthDay = parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd))%7;
 
 }
 
