84ee1f0eb4ed546af75b3d61ea71f65b8038e72a
Fridah-kalee <fridahkalee@gmail.com>
2022-03-06 11:35:21 +0300

    fixing changes on index.js

--- index.js
+++ index.js
@@ -12,10 +12,25 @@ function check(){
     var month = document.getElementById("month").value;
     var mm = parseInt(month);
     var year = document.getElementById("year").value;
-    var yy = parseInt(year.substr(2.4));
-    var cc = parseInt(year.substr(0.2));
+    var yy = parseInt(year.substr(2,4));
+    var cc = parseInt(year.substr(0,2));
 
-   var birthDay = parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd))%7;
+   var birthDay = parseInt(((cc/4) -2*cc-1) + ((5*yy/4)) + ((26*(mm+1)/10))+dd)%7;
 
-}
+   if (dd <=0 || dd>31){
+       alert("enter valid date");
+   }
+   else if(mm <=0 || mm>12){
+       alert("enter valid month");
+   }
+   else if(year <=0){
+       alert("enter valid year");
+   }
+   if (gender==="male"){
+       alert("your name is" + maleNames[birthDay]);
+   }
+   else if (gender==="female"){
+       alert("your name is" +femaleNames[birthDay]);
+   }
+  }
 
